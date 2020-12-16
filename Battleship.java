a029def7e98154d942b3a1d9bb0efaa4ea53b138
KEspaillat <71105698+KEspaillat@users.noreply.github.com>
2020-12-15 16:02:53 -0800

    Add files via upload

---
+++ Battleship.java
@@ -0,0 +1,14 @@
+package BattleshipApp;
+
+public class Battleship {
+	/** Main class in theproject which handles the game. **/
+	public static void main(String args[]) {
+		BattleshipModel model = new BattleshipModel();
+        BattleshipView view = new BattleshipView();
+		BattleshipController controller = new BattleshipController(model, view);
+		
+		controller.CreateField(model.home, model.enemy);		
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#BattleshipApp
+
+Single player Battleship game where the goal is to sink all the oppenents ships before they sinks you first.
